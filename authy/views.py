f29abf1006cd77f64cd668a326e7f2bb4a5c7d89
SUPREME-CODER <deep.bd@somaiya.edu>
2021-02-02 18:28:10 +0530

    Follow User

--- authy/views.py
+++ authy/views.py
@@ -7,11 +7,13 @@ from django.contrib.auth import update_session_auth_hash
 
 from authy.models import Profile
 from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 from django.core.paginator import Paginator
-from django.urls import resolve
-from post.models import Post, Follow
+from django.urls import resolve, reverse
+from post.models import Post, Follow, Stream
+
+from django.db import transaction
 
 # Create your views here.
 def UserProfile(request, username):
@@ -29,6 +31,7 @@ def UserProfile(request, username):
 	following_count = Follow.objects.filter(follower=user).count()
 	follower_count = Follow.objects.filter(following=user).count()
 
+	follow_status = Follow.objects.filter(following=user, follower=request.user).exists()
 
 	articles = profile.favorites.all()
 	paginator = Paginator(posts, 8)
@@ -44,6 +47,7 @@ def UserProfile(request, username):
 		'post_count': post_count,
 		'following_count': following_count,
 		'follower_count': follower_count,
+		'follow_status': follow_status,
 	}
 
 	return HttpResponse(template.render(context, request))
@@ -116,3 +120,25 @@ def EditProfile(request):
 	}
 
 	return render(request, 'edit_profile.html', context)
+
+
+@login_required
+def follow(request, username, option):
+	user = request.user
+	following = get_object_or_404(User, username=username)
+
+	try:
+		f, created = Follow.objects.get_or_create(follower=user, following=following)
+
+		if int(option) == 0:
+			f.delete()
+			Stream.objects.filter(following=following, user=user).all().delete()
+		else:
+			posts = Post.objects.all().filter(user=following)[:10]
+
+			for post in posts:
+				stream = Stream(post=post, user=user, date=post.posted, following=following)
+				stream.save()
+		return HttpResponseRedirect(reverse('profile', args=[username]))
+	except User.DoesNotExist:
+		return HttpResponseRedirect(reverse('profile', args=[username]))
