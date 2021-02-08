eeaef39aaed23dd240c7581c9c6dfcf666a4b175
SUPREME-CODER <deep.bd@somaiya.edu>
2021-02-08 23:04:58 +0530

    Design DM View

--- direct/models.py
+++ direct/models.py
@@ -15,12 +15,13 @@ class Message(models.Model):
 	def send_message(from_user, to_user, body):
 		sender_message = Message(user=from_user, sender=from_user, recipient=to_user, body=body, is_read=True)
 		sender_message.save()
+
 		recipient_message = Message(user=to_user, sender=from_user, recipient=from_user, body=body)
 		recipient_message.save()
 
 		return sender_message
 
-	def get_message(user):
+	def get_messages(user):
 		users = []
 		messages = Message.objects.filter(user=user).values('recipient').annotate(last=Max('date')).order_by('-last')
 		for message in messages:
@@ -29,4 +30,5 @@ class Message(models.Model):
 				'last': message['last'],
 				'unread': Message.objects.filter(user=user, recipient__pk=message['recipient'], is_read=False).count()
 			})
+
 		return users
