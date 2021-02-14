c6fbd91b712565aaefb26226ad7cae4b3a0c9bd3
SUPREME-CODER <deep.bd@somaiya.edu>
2021-02-14 13:34:53 +0530

    Send Messages

--- direct/urls.py
+++ direct/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from direct.views import inbox
+from direct.views import inbox, Directs, SendDirect
 
 urlpatterns = [
 	path('', inbox, name='inbox'),
+	path('directs/<username>', Directs, name='directs'),
+	path('send/', SendDirect, name='send_direct'),
 ]
--- direct/views.py
+++ direct/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
 from django.template import loader 
 
 # models
@@ -33,3 +33,38 @@ def inbox(request):
 	template = loader.get_template('direct.html')
 
 	return HttpResponse(template.render(context, request))
+
+@login_required
+def Directs(request, username):
+	user = request.user
+	messages = Message.objects.filter(user=user)
+	active_direct = username
+	directs = Message.objects.filter(user=user, recipient_username=username)
+	directs.update(is_read = True)
+
+	for message in messages:
+		if message['user'].username == username:
+			message['unread'] = 0
+
+	context = {
+		'directs' : directs,
+		'messages': messages,
+		'active_direct':active_direct,
+	}
+
+	template = loader.get_template('direct.html')
+
+	return HttpResponse(template.render(context, request))
+
+@login_required
+def SendDirect(request):
+	from_user = request.user
+	to_user_username = request.POST.get('to_user')
+	body = request.POST.get('body')
+
+	if request.method == 'POST':
+		to_user = User.objects.get(username=to_user_username)
+		Message.send_message(from_user, to_user, body)
+		return redirect('inbox')
+	else:
+		HttpResponseBadRequest()
