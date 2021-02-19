74743b7bcced554b8af9643e09034963ccc4950f
SUPREME-CODER <deep.bd@somaiya.edu>
2021-02-19 18:23:37 +0530

    Search User Navbar Bug

--- direct/urls.py
+++ direct/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from direct.views import inbox, Directs, SendDirect
+from direct.views import inbox, Directs, SendDirect, UserSearch
 
 urlpatterns = [
 	path('', inbox, name='inbox'),
 	path('directs/<username>', Directs, name='directs'),
 	path('send/', SendDirect, name='send_direct'),
+	path('new/', UserSearch, name='usersearch'),
 ]
